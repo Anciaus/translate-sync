e6cf87a89618460a7bd8c57ef27900e29b2f199f
Justas Ragauskas <justas.ragauskas@gmail.com>
2020-02-09 13:54:08 +0200

    Update landing page to use Angular material components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,17 @@ import { HomeRoutingModule } from './home-routing.module';
 
 import { SharedModule } from '../shared/shared.module';
 import { LandingComponent } from './landing/landing.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [LandingComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  imports: [
+    CommonModule,
+    SharedModule,
+    HomeRoutingModule,
+    MatButtonModule,
+    MatTableModule
+  ]
 })
 export class HomeModule {}
--- src/app/home/landing/landing.component.ts
+++ src/app/home/landing/landing.component.ts
@@ -6,7 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  public translationsSource: Translation[] = TRANSLATION_VALUES;
+  public displayedColumns: string[] = ['key', 'en', 'de', 'lt'];
+
   constructor() {}
 
   ngOnInit(): void {}
 }
+
+export interface Translation {
+  key: string;
+  [lang: string]: string;
+}
+
+export const TRANSLATION_VALUES: Translation[] = [
+  {
+    key: 'ALL',
+    en: 'All',
+    de: 'Alle',
+    lt: 'Visi'
+  },
+  {
+    key: 'NEW',
+    en: 'New',
+    de: 'Neu',
+    lt: 'Naujas'
+  }
+];
