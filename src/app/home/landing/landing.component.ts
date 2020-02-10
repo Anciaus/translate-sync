bff14bdef725270a7d5f990109ad26b13bd26e8a
Justas Ragauskas <justas.ragauskas@gmail.com>
2020-02-10 17:55:08 +0200

    Open file select dialog on button click

--- src/app/home/landing/landing.component.ts
+++ src/app/home/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FileSystem } from '@angular/compiler-cli/src/ngtsc/file_system';
+import Remote = Electron.Remote;
 
 export interface Translation {
   key: string;
@@ -29,7 +31,36 @@ export class LandingComponent implements OnInit {
   public translationsSource: Translation[] = TRANSLATION_VALUES;
   public displayedColumns: string[] = ['key', 'en', 'de', 'lt'];
 
-  constructor() {}
+  private remote: Remote;
+  private fs: FileSystem;
+
+  constructor() {
+    if ((<any>window).require) {
+      try {
+        this.remote = (<any>window).require('electron').remote;
+        this.fs = (<any>window).require('electron').fs;
+      } catch (e) {
+        throw e;
+      }
+    } else {
+      console.warn('App not running inside Electron!');
+    }
+  }
 
   ngOnInit(): void {}
+
+  public showUploadDialog(): void {
+    const { dialog } = this.remote;
+
+    dialog
+      .showOpenDialog({
+        title: 'Open translation file',
+        filters: [{ name: 'JSON', extensions: ['json'] }]
+      })
+      .then((value) => {
+        if (!value.canceled) {
+          console.log(value.filePaths);
+        }
+      });
+  }
 }
